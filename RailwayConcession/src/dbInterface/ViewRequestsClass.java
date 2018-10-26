31dee3d1832f990ee8a3f456d947a470eeeefcf3
prembhajaj <prembhajaj@gmail.com>
2018-10-27 00:50:10 +0530

    "View pending requests form completed"

--- RailwayConcession/src/dbInterface/ViewRequestsClass.java
+++ RailwayConcession/src/dbInterface/ViewRequestsClass.java
@@ -5,12 +5,34 @@
  */
 package dbInterface;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Pratik Panchal
  */
 public class ViewRequestsClass {
-    public static boolean changeStatus(String rcid, int status){
+
+    public static boolean changeStatus(String rcid, int status) {
+        String s = "";
+        try {
+            PreparedStatement st = CommonDataSet.conn.prepareStatement("call status_change(" + rcid + ", " + status + ")");
+            ResultSet rs=st.executeQuery();
+            while(rs.next())
+            {
+                s=rs.getString(1);
+            }
+            if(!s.equals("true")){
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ViewRequestsClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         return true;
     }
 }
