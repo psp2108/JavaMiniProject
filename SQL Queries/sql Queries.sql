3b38f687e757cc3e67770a5beecb7aa0476453bf
ruchashinde <ruchashinde139@gmail.com>
2018-10-26 18:56:25 +0530

    "tables railwayconcess modified"

--- SQL Queries/sql Queries.sql
+++ SQL Queries/sql Queries.sql
@@ -17,12 +17,12 @@ PRIMARY KEY(sap_id)
 	CREATION OF railwayconcession table
 create table rail_concess(
 sap_id int NOT NULL,
-rc_id int NOT NULL,
+rc_id int NOT NULL AUTO_INCREMENT,
 request_date date,
 class_type int,
 approved_from date,
 approved_till date,
-status int,
+status int DEFAULT 0,
 PRIMARY KEY(rc_id),
 FOREIGN KEY (sap_id) REFERENCES student_profile(sap_id)
 );
